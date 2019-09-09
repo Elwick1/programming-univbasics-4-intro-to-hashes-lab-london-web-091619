47257318d5ce636c60081580465dd7ddafdf0831
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 12:09:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,13 +24,14 @@ end
 
 #Create a method call read_from_hash that takes in two parameters. The first parameter is a hash, the second is a key. Used together, they will either produce a value on that hash corresponding to the key, or nil by default. Use these two parameters in conjunction to do just that.
 
-def read_from_hash(hash, key)
+#def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  read_from_hash({name: "Steve"}, :name)
-end
+ # read_from_hash
+#end
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  
 end
